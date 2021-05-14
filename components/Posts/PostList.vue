0489ad6fab2886f61d2298794240b5c309489135
anna <anna49.fog@yandex.ru>
2021-05-14 15:51:55 +0300

    initial commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="posts-list">
+    <PostPreview v-for="post in posts" :post="post" :key="post.id" />
+  </section>
+</template>
+
+<script>
+import PostPreview from '~/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  },
+  data () {
+    return {
+      posts: [
+        {
+          id: 1,
+          thumbnail: 'https://www.bbva.com/wp-content/uploads/2017/08/innovacion-bbva-100817.jpg',
+          title: 'Title 1',
+          previewText: 'This is a post'
+        },
+        {
+          id: 2,
+          thumbnail: 'https://www.orfonline.org/wp-content/uploads/2020/09/tech-1280x720.jpg',
+          title: 'Title 2',
+          previewText: 'This is another post'
+        },
+        {
+          id: 3,
+          thumbnail: 'https://www.bbva.com/wp-content/uploads/2017/08/innovacion-bbva-100817.jpg',
+          title: 'Title 3',
+          previewText: 'This is a third post'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.posts-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,67 @@
+<template>
+  <nuxt-link :to="'/posts/' + post.id" class="post-preview">
+    <article>
+      <div
+        class="post-thumbnail"
+        :style="{backgroundImage: `url('${post.thumbnail}')`}">
+      </div>
+      <div class="post-content">
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.previewText }}</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+  margin-bottom: 20px;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+
+
+</style>
