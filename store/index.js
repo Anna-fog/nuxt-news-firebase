ea742bcb2e407fa07cc15c7cad0a95a92e1fe4ab
anna <anna49.fog@yandex.ru>
2021-05-18 12:04:39 +0300

    update initAuth method

--- store/index.js
+++ store/index.js
@@ -1,3 +1,5 @@
+import Cookie from 'js-cookie'
+
 export default {
   state() {
     return {
@@ -78,9 +80,15 @@ export default {
         returnSecureToken: true
       }).then(result => {
         commit("setToken", result.idToken);
-        dispatch('setLogoutTimer', result.expiresIn  * 1000);
+        localStorage.setItem('token', result.idToken);
+        localStorage.setItem('tokenExpiration', new Date().getTime() + result.expiresIn * 1000)
 
+        Cookie.set('jwt', result.idToken);
+        Cookie.set('expirationDate', new Date().getTime() + result.expiresIn * 1000);
+
+        dispatch('setLogoutTimer', result.expiresIn  * 1000);
         })
+
         .catch(e => console.log(e))
     },
 
@@ -88,6 +96,36 @@ export default {
       setTimeout(() => {
         commit('clearToken');
       }, duration)
+    },
+
+    initAuth({ commit, dispatch }, req) {
+      let token;
+      let expirationDate;
+
+      if (req) {
+        if (!req.headers.cookie) return;
+
+        const jwtCookie = req.headers.cookie
+          .split(';')
+          .find(c => c.trim().startsWith('jwt='));
+        if (!jwtCookie) return;
+        token = jwtCookie.split('=')[1];
+
+        expirationDate = req.headers.cookie
+          .split(';')
+          .find(c => c.trim().startsWith('expirationDate='))
+          .split('=')[1];
+      } else {
+        token = localStorage.getItem('token');
+        expirationDate = localStorage.getItem('tokenExpiration');
+
+        if (new Date().getTime() > +expirationDate || !token) return;
+      }
+
+      dispatch('setLogoutTimer', +expirationDate - new Date().getTime());
+      commit('setToken', token);
+
+      // console.log(+expirationDate - new Date().getTime())
     }
   },
 
